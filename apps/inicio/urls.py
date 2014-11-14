0c6f36c4e587fb108558e8bee2624eb553c2c2fd
Amaur <amaurice@am.amauri@outlook.com>
2014-11-14 20:57:29 -0200

    working on

--- apps/inicio/urls.py
+++ apps/inicio/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from .views import Index
+from .views import Index,Search,About , Cat, Archive
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,5 +9,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'Lohinlo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^index',Index),
+    url(r'^search', Search),
+    url(r'^about', About),
+    url(r'^categ', Cat),
+    url(r'^archives', Archive),
 
 )
--- apps/inicio/views.py
+++ apps/inicio/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 
 def Index(request):
     return render(request, 'inicio/index.html')
+
+def About(request):
+    return render(request, 'inicio/about.html')
+
+def Cat(request):
+    return render(request, 'inicio/cat.html')
+
+def Archive(request):
+    return render(request, 'inicio/archive.html')
+
+
+def Search(request):
+    if 'q' in request.GET and request.GET['q']:
+        q = request.GET['q']
+        print "Done..."
+        message = q
+    else:
+        print "Failed..!"
+        message = 'Failed..!'
+    
+    return render(request, 'inicio/search.html', {'mes':message})
+    
+
